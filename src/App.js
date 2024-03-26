329ddce42287aa9614abe6866bd7689626e6e403
Kavin Gnanapandithan <kavin.balag@gmail.com>
2024-03-26 16:29:50 -0400

    Added Navbar using Tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h1>Fairmont Hotels</h1>
     </div>
   );
