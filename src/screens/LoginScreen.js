6b6670b1cdf8802e252832230b6c796b1a984895
Kavin Gnanapandithan <kavin.balag@gmail.com>
2024-03-30 22:31:21 -0400

    Added back-end call to login page

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,7 @@
+import axios from 'axios'
 import React from 'react'
 import {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
 
 function LoginScreen() {
   const[name, setname] =useState('')
@@ -7,34 +9,43 @@ function LoginScreen() {
   const[password, setpassword] = useState('')
   const[cpassword, setcpassword] = useState('')
 
-  function login(){
+  const [emailError, setEmailError] = useState('')
 
-          const user={
-              email,
-              password
-          }
-          console.log(user)
-      }
+
+  function login(email, password)  {
+    axios.get(`http://localhost:3001/api/customer/mail?customer_address=${email}&password=${password}`)
+      .then(response => {
+        if (response.data.length != 0) {
+          window.location.href = '/';
+        } else {
+          setEmailError("Invalid email or password");
+        }
+      })
+  }
+
+
+
   
-return (
-  <div>
-      <div className='row justify-content-center mt-5'>
-          <div className='col-md-5 mt-5'>
+  return (
+    <div>
+        <div className='row justify-content-center mt-5'>
+            <div className='col-md-5 mt-5'>
 
-              <div className='bs'>
-                  <h2 >Login</h2>
+                <div className='bs'>
+                    <h2 >Login</h2>
 
-                  <input type='text' className='form-control' placeholder='email'
-                  value = {email} onChange={(e)=>{setemail(e.target.value)}}></input>
+                    <input type='text' className='form-control' placeholder='email'
+                    value = {email} onChange={(e)=>{setemail(e.target.value)}}></input>
 
-                  <input type='text' className='form-control' placeholder='password'
-                  value = {password} onChange={(e)=>{setpassword(e.target.value)}}></input>
+                    <input type='text' className='form-control' placeholder='password'
+                    value = {password} onChange={(e)=>{setpassword(e.target.value)}}></input>
 
-                  <button className='btn-secondary' onClick={login}>Login</button>
-              </div>
-          </div>
-      </div>
-  </div>
-)
+                    <button className='btn-secondary' onClick={login(email, password)}>Login</button>
+                    <p>{emailError}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
 }
 export default LoginScreen
